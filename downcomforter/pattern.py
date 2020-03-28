82fc687470d5cd5333d541bf620945c0cb80cd88
Jonathan Bowman <git@bowmanjd.com>
2020-03-28 05:42:30 -0400

    mkdocs initial config and flake8 update

--- downcomforter/pattern.py
+++ downcomforter/pattern.py
@@ -1,15 +1,16 @@
 # Copyright 2020 Jonathan Bowman
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-# this file except in compliance with the License. You may obtain a copy of the
-# License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software distributed
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
 import functools
 import importlib
@@ -20,11 +21,11 @@ from . import matter
 from . import down
 from . import highlight
 
-brace_re = re.compile(r"{([^}]+)}")
-tpl_re = re.compile(r"(?!{content}){([^}]+)}")
+BRACE_RE = re.compile(r"{([^}]+)}")
+TPL_RE = re.compile(r"(?!{content}){([^}]+)}")
 
 
-def escape_braces(text, pattern=brace_re):
+def escape_braces(text, pattern=BRACE_RE):
     escaped = pattern.sub(r"{{\1}}", text)
     return escaped
 
@@ -63,46 +64,66 @@ def call_helpers(conf):
     return conf
 
 
+def merge(conf, content):
+    conf = call_helpers(conf)
+    return content.format(**conf)
+
+
+def parse_includes():
+    pass
+
+
+def prepare_includes(conf, directory):
+    includes = conf.get("include", {})
+    try:  # get next filename from includes, if any
+        filename, label = includes.popitem()
+        filepath = (directory / filename).resolve()
+        includes[filepath] = label
+    except KeyError:
+        filepath = None
+    return filepath, conf
+
+
+def get_include_label(filename, conf):
+    try:
+        label = conf["include"].pop(filename)
+    except KeyError:
+        label = None
+    return label
+
+
 def loader(filename=None, conf={}, content=""):
     """Load file, related files, and build document"""
     # Done if no more files
     if filename is None:
-        conf = call_helpers(conf)
-        return content.format(**conf)
+        return conf, content
     p = Path(filename)
-    print(p)
     # Split front matter and raw content
     new_conf, new_content = matter.load_matter(p)
     # ensure raw content is now html
     if p.suffix == ".md":
         new_content = codedown(new_content)
     # check if this filename was included by previous
-    try:
-        label = conf["include"].pop(filename)
-    except KeyError:  # not an include
+    label = get_include_label(filename, conf)
+    if label is None:  # not an include
         conf = {**conf, **new_conf}
         content = new_content
-    else:  # if this is include, then:
-        if label != "parent":  # refers to child snippet
-            # assign to template var by label; merge in child vars
-            conf = {label: new_content, **conf, **new_conf}
-        else:  # refers to parent template
-            # place existing content within loaded new template
-            template = escape_braces(new_content, tpl_re)
-            content = template.format(content=content)
-            # merge previous vars into template vars
-            conf = {**new_conf, **conf}
+    elif label == "parent":  # refers to parent template
+        # place existing content within loaded new template
+        template = escape_braces(new_content, TPL_RE)
+        content = template.format(content=content)
+        # merge previous vars into template vars
+        conf = {**new_conf, **conf}
+    else:  # refers to child snippet
+        # assign to template var by label; merge in child vars
+        conf = {label: new_content, **conf, **new_conf}
+
     # check front matter for helpers
-    prepare_helpers(conf, p.parent)
+    conf = prepare_helpers(conf, p.parent)
 
     # check front matter for includes
-    includes = conf.get("include", {})
-    try:  # get next filename from includes, if any
-        filename, label = includes.popitem()
-        filepath = (p.parent / filename).resolve()
-        includes[filepath] = label
-    except KeyError:
-        filepath = None
+    filepath, conf = prepare_includes(conf, p.parent)
+
     return loader(filepath, conf, content)
 
 
--- downcomforter/preview.py
+++ downcomforter/preview.py
@@ -1,15 +1,16 @@
 # Copyright 2020 Jonathan Bowman
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-# this file except in compliance with the License. You may obtain a copy of the
-# License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software distributed
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
 import sys
 import importlib.resources
@@ -23,6 +24,8 @@ from . import pattern
 
 
 def make_app(mdfilename, cssfilename=None, tplfilename=None):
+    """Makes a WSGI app"""
+
     def app(env, start_response):
         status = "200 OK"
         content_type = "text/html"
@@ -32,18 +35,14 @@ def make_app(mdfilename, cssfilename=None, tplfilename=None):
             if cssfilename is None:
                 output = importlib.resources.read_text(__package__, "style.css")
             else:
-                with open(cssfilename) as f:
-                    output = f.read()
+                with open(cssfilename) as handle:
+                    output = handle.read()
                 content_type = "text/css"
         elif env.get("PATH_INFO").endswith("favicon.ico"):
             status = "404 Not Found"
 
         else:
-            # conf, content = matter.load_matter(mdfilename)
-            # coded = highlight.codedoc(content)
-            # html = down.md_to_html(coded, tplfilename)
-            # output = html.format(**conf).encode("utf-8")
-            output = pattern.loader(mdfilename)
+            output = pattern.merge(*pattern.loader(mdfilename))
 
         output = output.encode("utf-8")
         start_response(
@@ -59,10 +58,17 @@ def make_app(mdfilename, cssfilename=None, tplfilename=None):
     return app
 
 
-if __name__ == "__main__":
+def serve(filename, host="0.0.0.0"):
+    server = Server(make_app(filename, css))
+    server.watch(filename)
+    server.serve(host=host)
+
+
+def run():
     md = sys.argv[1]
     css = sys.argv[2]
-    print(md)
-    server = Server(make_app(md, css))
-    server.watch(md)
-    server.serve(host="0.0.0.0")
+    serve()
+
+
+if __name__ == "__main__":
+    run()
